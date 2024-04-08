f8ec9a432b0869a7bdab893529c5c37bce61ffe5
ximenaconde04 <164540961+ximenaconde04@users.noreply.github.com>
2024-04-08 16:18:01 -0600

    Update settings.py

--- Universidad/settings.py
+++ Universidad/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Aplicaciones.Academico'
+    'Aplicaciones.Academico.apps.AcademicoConfig'
 ]
 
 MIDDLEWARE = [
